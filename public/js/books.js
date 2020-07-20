d7179ae3a23a930448c0167f0f23d88908c4fd3f
Jorge Guzman <jorguzman100@gmail.com>
2020-07-20 06:10:32 +0300

    Testing html routes for navigation bar

---
+++ public/js/books.js
@@ -0,0 +1,104 @@
+$(document).ready(() => {
+
+    // ********** Event listeners ***********
+
+    $(document).on('click', (event) => {
+
+        // Click on any category name
+        if ($(event.target).attr('class') === 'categoryLink') {
+
+            // Get the books within the category
+            $.ajax({
+                method: "GET",
+                url: `/api/books/category/${event.target.text}`
+            }).then((books) => {
+                console.log(`Books within ${event.target.text}: `, books);
+
+                // Empty the booksCards area
+                $('#booksCards').empty();
+
+                // Create the book cards
+                books.forEach((book) => {
+                    // data-toggle="modal" data-target="#bookModal"
+                    let card = `
+                <div class="card" style="width: 12rem;">
+                    <img src="${book.thumbnail}" class="card-img-top" alt="${book.title} book image" />
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="#" class="modalTrigger" bookId="${book.id}">${book.title}</a></h5>
+                        <p class="card-text">
+                            By ${book.authors}
+                        </p>
+                    </div>
+                </div>
+                `;
+                    $('#booksCards').append(card);
+                });
+            });
+        }
+
+        // Click on the modal
+        else if ($(event.target).attr('class') == 'modalTrigger') {
+
+            // Get book details by its id
+            $.ajax({
+                method: "GET",
+                url: `/api/books/${$(event.target).attr('bookId')}`
+            }).then((book) => {
+                console.log('Modal book: ', book);
+
+                // Create the modal book card
+                let modalBookCard = `
+                <div class="card">
+                    <img src="${book.thumbnail}" class="card-img-top" alt="${book.thumbnail}" />
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="#" class="modalTrigger" bookId="${book.id}">${book.title}</a></h5>
+                        <p class="card-text"> <strong> Subtitle </strong> ${book.subtitle}</p>
+                        <p class="card-text"> <strong> By </strong> ${book.authors}</p>
+                        <p class="card-text"> <strong> Categories </strong> ${book.categories}</p>
+                        <p class="card-text"> <strong> Published year </strong> ${book.published_year}</p>
+                        <p class="card-text"> <strong> Average rating </strong> ${book.average_rating}</p>
+                        <p class="card-text"> <strong> Ratings count </strong> ${book.ratings_count}</p>
+                        <p class="card-text"> <strong> Description </strong> ${book.description}<p>
+                        <p class="card-text"> <strong> Price </strong> ${book.price}</p>
+                    </div>
+                </div>
+                `;
+                $('.modal-body').append(modalBookCard);
+
+                // Display the modal
+                $('#bookModal').modal();
+            });
+        }
+    });
+
+
+
+
+    // Load the books
+    $.ajax({
+        method: "GET",
+        url: "/api/books/"
+    }).then((books) => {
+        console.log('Books: ', books);
+
+        // Create the list of categories
+        let categories = books.map((book) => {
+            return book.categories;
+        });
+        console.log('Categories: ', categories);
+
+        let uniqueCategories = Array.from(new Set(categories));
+
+        console.log('uniqueCategories: ', uniqueCategories);
+
+        uniqueCategories.forEach((category) => {
+            let li = $('<li>');
+            let a = $('<a>');
+            a.attr('href', `#`);
+            a.attr('class', 'categoryLink');
+            a.text(category);
+            li.append(a);
+            $('#categories').append(li);
+        });
+    });
+});
