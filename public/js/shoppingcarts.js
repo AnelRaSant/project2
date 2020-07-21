ddb2e6cb94b2b202fd74d2d7286d2c49d8a03b5e
Jorge Guzman <jorguzman100@gmail.com>
2020-07-21 11:57:07 +0300

    Testing routes - Purchase history table displayed

--- public/js/shoppingcarts.js
+++ public/js/shoppingcarts.js
@@ -1,7 +1,30 @@
 $(document).ready(() => {
+    $('#purchasesDiv').hide();
 
     // ********** Event listeners ***********
     $(document).on('click', (event) => {
+
+        // Continue browsing button clicked
+        if ($(event.target).attr('id') === 'continueBrowsing') {
+            window.location.href = "/browse"
+        }
+
+        // Purchases history button clicked
+        if ($(event.target).attr('id') === 'purchasesHistory') {
+            $('#purchasesDiv').toggle();
+        }
+
+        // Confirm purchse button clicked
+        if ($(event.target).attr('id') === 'confirmPurchase') {
+            // Display the modal
+            $('#purchaeConfirmationModal').modal();
+
+            // Show purchase history
+            $('#purchaeConfirmationModal').on('hidden.bs.modal', function (e) {
+                $('#purchasesDiv').show();
+            })
+        }
+
     });
 
     // Load the shoppingcarts
@@ -10,7 +33,6 @@ $(document).ready(() => {
         url: "/api/shoppingcarts/"
     }).then((shoppingcarts) => {
         let total = 0;
-        console.log('Shoppingcarts: ', shoppingcarts);
 
         // Clean the data
         let cleanedCarts = shoppingcarts.map((shoppingcart) => {
@@ -20,11 +42,9 @@ $(document).ready(() => {
                 Books: shoppingcart.Books
             }
         });
-        // console.log('cleanedCarts: ', cleanedCarts);
 
         // Create the table
         cleanedCarts.forEach((cart) => {
-            // console.log('cart: ', cart);
             let tr = $('<tr>');
             let td0 = $('<td>');
             let td1 = $('<td>');
@@ -34,21 +54,10 @@ $(document).ready(() => {
             td0.text(cart.id);
             td1.text(cart.UserId);
             Object.values(cart).forEach((cartElement) => {
-
-                // console.log(`cartElement: ${cartElement}, typeof ${typeof cartElement}`);
-
                 if (typeof cartElement === 'object' && cartElement != null && cartElement[0] != undefined) {
-                    // td.text(cartElement[0]);
                     td2.text(cartElement[0].id);
                     td3.text(cartElement[0].title);
                     td4.text(cartElement[0].price);
-                    /* let price = parseInt(cartElement[0].price);
-                    console.log('typeof cartElement[0].price: ', typeof cartElement[0].price);
-                    console.log('price: ', price);
-                    console.log('typeof price: ', typeof price);
-                    // total = parseInt(total + price);
-                    total = 500 + 500;
-                    console.log('total: ', total); */
                     total += parseFloat(cartElement[0].price);
                 }
                 tr.append(td0);
