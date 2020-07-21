112181355a75d9c85bff985529d38526c70416a2
Jorge Guzman <jorguzman100@gmail.com>
2020-07-21 03:27:52 +0300

    Testing routes - cart updated

--- public/js/shoppingcarts.js
+++ public/js/shoppingcarts.js
@@ -21,6 +21,7 @@ $(document).ready(() => {
         method: "GET",
         url: "/api/shoppingcarts/"
     }).then((shoppingcarts) => {
+        let total = 0;
         console.log('Shoppingcarts: ', shoppingcarts);
 
         // Clean the data
@@ -35,7 +36,7 @@ $(document).ready(() => {
 
         // Create the table
         cleanedCarts.forEach((cart) => {
-            console.log('cart: ', cart);
+            // console.log('cart: ', cart);
             let tr = $('<tr>');
             let td0 = $('<td>');
             let td1 = $('<td>');
@@ -49,10 +50,10 @@ $(document).ready(() => {
                 // console.log(`cartElement: ${cartElement}, typeof ${typeof cartElement}`);
 
                 if (typeof cartElement === 'object' && cartElement != null && cartElement[0] != undefined) {
-                    console.log('Book: ', cartElement[0]);
+                    /* console.log('Book: ', cartElement[0]);
                     console.log('Book.id: ', cartElement[0].id);
                     console.log('Book.title: ', cartElement[0].title);
-                    console.log('Book.price: ', cartElement[0].price);
+                    console.log('Book.price: ', cartElement[0].price); */
                     /* Object.entries(cartElement[0]).forEach((bookProp) => {
                         console.log('bookProp: ', bookProp);
                         td.text(bookProp);
@@ -61,6 +62,14 @@ $(document).ready(() => {
                     td2.text(cartElement[0].id);
                     td3.text(cartElement[0].title);
                     td4.text(cartElement[0].price);
+                    /* let price = parseInt(cartElement[0].price);
+                    console.log('typeof cartElement[0].price: ', typeof cartElement[0].price);
+                    console.log('price: ', price);
+                    console.log('typeof price: ', typeof price);
+                    // total = parseInt(total + price);
+                    total = 500 + 500;
+                    console.log('total: ', total); */
+                    total += parseFloat(cartElement[0].price);
                 }
                 tr.append(td0);
                 tr.append(td1);
@@ -70,23 +79,22 @@ $(document).ready(() => {
             });
             $('#cartTableBody').append(tr);
         });
+        total = total.toFixed(2);
+        let tr = $('<tr>');
+        let td0 = $('<td>');
+        let td1 = $('<td>');
+        let td2 = $('<td>');
+        let td3 = $('<td>');
+        let td4 = $('<td>');
+        td3.text('Total');
+        td4.text(`${total}`);
+        tr.append(td0);
+        tr.append(td1);
+        tr.append(td2);
+        tr.append(td3);
+        tr.append(td4);
+        $('#cartTableBody').append(tr);
     });
 
 
 });
-
-{/* <tr>
-    <th scope="col">ShoppingcartId</th>
-    <th scope="col">UserId</th>
-    <th scope="col">BookId</th>
-    <th scope="col">Title</th>
-    <th scope="col">Price</th>
-</tr>
-
-        <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr> */}
