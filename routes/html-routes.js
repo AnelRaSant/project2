899126b8d6bb58a8d33cfddd87a2ccb4926eb10f
AnelRaSant <anel.rmrz.stgo7@gmail.com>
2020-07-22 10:36:18 -0500

    added all handlebars, design and is working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,23 +13,30 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads home.html
+  //app.get("/", function(req, res) {
+  //res.sendFile(path.join(__dirname, "../public/views/home.handlebars"));
+  //});
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/home.html"));
+  res.render("home")
   });
+
 
   // browse route loads browse.html
   app.get("/browse", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/browse.html"));
+    res.render("browse")
+    //res.sendFile(path.join(__dirname, "../views/browse.handlebars"));
   });
 
   // cart route loads cart.html
   app.get("/cart", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/cart.html"));
+    res.render("cart")
+    //res.sendFile(path.join(__dirname, "../public/views/cart.html"));
   });
 
   // signupLogin route loads signupLogin.html
   app.get("/signupLogin", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/signupLogin.html"));
+    res.render("signupLogin")
+    //res.sendFile(path.join(__dirname, "../public/views/signupLogin.html"));
   });
 
 };
